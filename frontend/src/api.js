1bdbf543b576e2558701816e8b3c8b7cb056340a
Caleb <CSJ0014@gmail.com>
2025-10-24 18:50:09 -0400

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -23,3 +23,12 @@ export async function generateReport(fname) {
   const blob = await res.blob();
   return blob;
 }
+
+import axios from "axios";
+
+const API_BASE = "https://cyclingdashboard-backend.onrender.com/api";
+
+export const api = axios.create({
+  baseURL: API_BASE,
+  headers: { "Content-Type": "application/json" },
+});
